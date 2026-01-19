0f398a59863556b8aaf9fea69f38ebca6722f12f
slemanz <william_sleman@hotmail.com>
2026-01-19 04:35:17 -0300

    interface(feat): add timebase

--- interface/Inc/interface_defines.h
+++ interface/Inc/interface_defines.h
@@ -1,6 +1,19 @@
-#ifndef INC_INTERFACE_DEFINES_H_
-#define INC_INTERFACE_DEFINES_H_
+#ifndef INC_INTERFACE_INSTANCES_H_
+#define INC_INTERFACE_INSTANCES_H_
+
+/************************************************************
+*                   COMM INSTANCES                          *
+*************************************************************/
+
+#define INTERFACE_PROTOCOL_UART2            0
+
+/************************************************************
+*                     IO INSTANCES                          *
+*************************************************************/
+
+#define INTERFACE_IO_0          0       // P111
+#define INTERFACE_IO_1          1       // P304
+#define INTERFACE_IO_2          2
 
 
-
-#endif /* INC_INTERFACE_DEFINES_H_ */
+#endif /* INC_INTERFACE_INSTANCES_H_ */
