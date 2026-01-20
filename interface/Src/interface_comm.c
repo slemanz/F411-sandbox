1e94521eba68c42e7987b6cc469af2baea6f9587
slemanz <william_sleman@hotmail.com>
2026-01-20 04:22:41 -0300

    interface(feat): add protocol uart2

--- interface/Src/interface_comm.c
+++ interface/Src/interface_comm.c
@@ -7,7 +7,7 @@ Comm_Interface_t *Comm_ProtocolGet(uint8_t type)
 {
     switch (type)
     {
-        case INTERFACE_PROTOCOL_UART2: return (Comm_Interface_t*)&sci2_protocol;
+        case INTERFACE_PROTOCOL_UART2: return (Comm_Interface_t*)&uart2_protocol;
         default: return NULL;
     }
 }
--- interface/Src/protocol_uart.c
+++ interface/Src/protocol_uart.c
@@ -13,27 +13,57 @@
 *                       SCI2                                *
 *************************************************************/
 
-static ring_buffer_t rb_sci2 = {0U};
-static uint8_t data_buffer_sci2[SERIAL_BUFFER_SIZE] = {0U};
-static uint8_t sci2_is_init = 0;
+static ring_buffer_t rb_uart2 = {0U};
+static uint8_t data_buffer_uart2[SERIAL_BUFFER_SIZE] = {0U};
+static uint8_t uart2_is_init = 0;
 
-void sci2_protocol_init(void)
+void uart2_protocol_init(void)
 {
-    ring_buffer_setup(&rb_sci2, data_buffer_sci2, SERIAL_BUFFER_SIZE);
+    ring_buffer_setup(&rb_uart2, data_buffer_uart2, SERIAL_BUFFER_SIZE);
 
-    sci2_is_init = 1;
+    GPIO_PinConfig_t uart_pin;
+    uart_pin.pGPIOx = GPIOA;
+    uart_pin.GPIO_PinNumber = GPIO_PIN_NO_3;
+    uart_pin.GPIO_PinMode = GPIO_MODE_ALTFN;
+    uart_pin.GPIO_PinSpeed = GPIO_SPEED_LOW;
+    uart_pin.GPIO_PinOPType = GPIO_OP_TYPE_PP;
+    uart_pin.GPIO_PinPuPdControl = GPIO_NO_PUPD;
+    uart_pin.GPIO_PinAltFunMode = PA3_ALTFN_UART2_RX;
+    GPIO_Init(&uart_pin);
+
+    uart_pin.pGPIOx = GPIOA;
+    uart_pin.GPIO_PinNumber = GPIO_PIN_NO_2;
+    uart_pin.GPIO_PinAltFunMode = PA2_ALTFN_UART2_TX;
+    GPIO_Init(&uart_pin);
+
+    UART_Config_t uart_config;
+    uart_config.pUARTx = UART2;
+    uart_config.UART_Mode = UART_MODE_TXRX;
+    uart_config.UART_Baud = UART_STD_BAUD_115200;
+    uart_config.UART_NoOfStopBits = UART_STOPBITS_1;
+    uart_config.UART_WordLength = UART_WORDLEN_8BITS;
+    uart_config.UART_ParityControl = UART_PARITY_DISABLE;
+    uart_config.UART_HWFlowControl = UART_HW_FLOW_CTRL_NONE;
+
+    UART_Init(&uart_config);
+
+    UART_InterruptConfig(UART2, UART_INTERRUPT_RXNEIE, ENABLE);
+    interrupt_Config(IRQ_NO_UART2, ENABLE);
+    UART_PeripheralControl(UART2, ENABLE);
+
+    uart2_is_init = 1;
 }
 
 
-void sci2_protocol_send(uint8_t *data, uint32_t Len)
+void uart2_protocol_send(uint8_t *data, uint32_t Len)
 {
-    if(!sci2_is_init) sci2_protocol_init();
-    //SCI_Write(SCI2, data, Len);
+    if(!uart2_is_init) uart2_protocol_init();
+    UART_write(UART2, data, Len);
 }
 
-uint8_t sci2_protocol_receive(uint8_t *buffer, uint32_t Len)
+uint8_t uart2_protocol_receive(uint8_t *buffer, uint32_t Len)
 {
-    if(!sci2_is_init) sci2_protocol_init();
+    if(!uart2_is_init) uart2_protocol_init();
 
     if(Len == 0)
     {
@@ -42,24 +72,26 @@ uint8_t sci2_protocol_receive(uint8_t *buffer, uint32_t Len)
 
     for(uint8_t bytes_read = 0; bytes_read < Len; bytes_read++)
     {
-        if(!ring_buffer_read(&rb_sci2, &buffer[bytes_read])) return bytes_read;
+        if(!ring_buffer_read(&rb_uart2, &buffer[bytes_read])) return bytes_read;
     }
 
     return Len;
 }
 
-uint8_t sci2_protocol_data_available(void)
+uint8_t uart2_protocol_data_available(void)
 {
-    if(!sci2_is_init) sci2_protocol_init();
-    return !ring_buffer_empty(&rb_sci2);
+    if(!uart2_is_init) uart2_protocol_init();
+    return !ring_buffer_empty(&rb_uart2);
 }
 
-void EVENT0_IRQHandler(void)
+void USART2_IRQHandler(void)
 {
-    uint8_t received_data;
-    //SCI_ReadByte(SCI2, &received_data);
-    ring_buffer_write(&rb_sci2, received_data);
-    //icu_clear_irq(ICU_EVENT_NO_0);
+	//const bool overrun_occurred = UART_GetFlagStatus(UART2, UART_FLAG_ORE);
+	//const bool received_data = UART_GetFlagStatus(UART2, UART_FLAG_RXNE);
+
+	//if(received_data || overrun_occurred)
+    uint8_t data = UART2->DR;
+    ring_buffer_write(&rb_uart2, data);
 }
 
 
@@ -67,10 +99,10 @@ void EVENT0_IRQHandler(void)
 *                     WRAPPER                               *
 *************************************************************/
 
-const Comm_Interface_t sci2_protocol = {
-    .init = sci2_protocol_init,
-    .send = sci2_protocol_send,
-    .receive = sci2_protocol_receive,
-    .data_available = sci2_protocol_data_available,
+const Comm_Interface_t uart2_protocol = {
+    .init = uart2_protocol_init,
+    .send = uart2_protocol_send,
+    .receive = uart2_protocol_receive,
+    .data_available = uart2_protocol_data_available,
     .deinit = NULL
 };
