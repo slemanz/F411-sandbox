944a3f11bab7f435878634d8394f375a6f740758
slemanz <william_sleman@hotmail.com>
2026-01-21 05:04:27 -0300

    common(feat): add pool structures

---
+++ common/Inc/core/pool.h
@@ -0,0 +1,26 @@
+#ifndef INC_POOL_H_
+#define INC_POOL_H_
+
+#include <stdint.h>
+#include <stddef.h>
+
+/* Configuration parameters*/
+
+#define POOL_MEMORY_SIZE        20 // number of memory pools
+#define POOL_BLOCK_SIZE         32 // Size of each block in bytes
+#define POOL_ALIGNAMENT         4  // Align memory blocks to 4 bytes
+
+/* Memory pool data structure */
+
+typedef struct MemBlock{
+    struct MemBlock *next;
+}MemBlock_t;
+
+typedef struct
+{
+    uint8_t pool[POOL_MEMORY_SIZE][POOL_BLOCK_SIZE]; // Pre-allocated memory
+    MemBlock_t *freelist; // Linked list of free block
+}MemPool_t;
+
+
+#endif /* INC_POOL_H_ */
