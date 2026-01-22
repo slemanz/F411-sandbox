958938a1247477a7c7d8815ab15efd0ba1a7b729
slemanz <william_sleman@hotmail.com>
2026-01-22 05:16:02 -0300

    common(refact): improve led implemantation

--- common/Src/bsp/led.c
+++ common/Src/bsp/led.c
@@ -1,13 +1,50 @@
 #include "bsp/led.h"
+#include "core/uprint.h"
+#include "shared/pool.h"
 
-static IO_Interface_t *led_pin;
+struct led_t{
+    const char *name;
+    IO_Interface_t *pin;
+};
 
-void led_setup(IO_Interface_t *io_pin)
+ledPtr_t led_create(const char *name, IO_Interface_t *io_pin)
 {
-    led_pin = io_pin;
+    ledPtr_t led = (ledPtr_t)pool_Allocate();
+
+    if(led)
+    {
+        led->name = name;
+        led->pin = io_pin;
+    }else
+    {
+        uprint("Low memory, cannot create device\n");
+    }
+
+    return led;
 }
 
-void led_toggle(void)
+void led_turn_on(ledPtr_t led)
 {
-    if(led_pin != NULL) led_pin->toggle();
+    led->pin->write(1);
+}
+
+void led_turn_off(ledPtr_t led)
+{
+    led->pin->write(0);
+}
+
+void led_toggle(ledPtr_t led)
+{
+    led->pin->toggle();
+}
+
+void led_destroy(ledPtr_t led)
+{
+    uprint("*** %s destroyed***\n\r", led->name);
+    pool_free(led);
+}
+
+void led_displayInfo(ledPtr_t led)
+{
+
 }
