cc2f68a283ec9655a2ec82a396e037e45334d167
slemanz <william_sleman@hotmail.com>
2026-01-22 05:15:31 -0300

    common(refact): change led interface

--- common/Inc/bsp/led.h
+++ common/Inc/bsp/led.h
@@ -4,7 +4,16 @@
 #include <stdint.h>
 #include "interface/interface.h"
 
-void led_setup(IO_Interface_t *io_pin);
-void led_toggle(void);
+typedef struct led_t* ledPtr_t;
+
+ledPtr_t led_create(const char *name, IO_Interface_t *io_pin);
+
+void led_turn_on(ledPtr_t led);
+void led_turn_off(ledPtr_t led);
+void led_toggle(ledPtr_t led);
+
+void led_destroy(ledPtr_t led);
+void led_displayInfo(ledPtr_t led);
+
 
 #endif /* INC_LED_H_ */
