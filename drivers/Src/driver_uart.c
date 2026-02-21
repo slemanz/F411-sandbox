91f8fe64e43a3ffd846906ffef3bde1daa39ded1
slemanz <william_sleman@hotmail.com>
2026-02-21 14:43:29 -0300

    drivers(refact): add uart private functions

--- drivers/Src/driver_uart.c
+++ drivers/Src/driver_uart.c
@@ -1,162 +1,85 @@
 #include "driver_uart.h"
 #include "driver_clock.h"
+#include "driver_systick.h"
 
+/*********************************************************************
+ * Private Helper Functions
+ *********************************************************************/
 
-static uint16_t compute_uart_div(uint32_t PeriphClk, uint32_t BaudRate);
+ static uint16_t compute_baud_div(uint32_t peripheralClk, uint32_t baudRate)
+ {
+    return (uint16_t)((peripheralClk + (baudRate/2U))/baudRate);
+ }
 
-static uint16_t compute_uart_div(uint32_t PeriphClk, uint32_t BaudRate)
-{
-    return ((PeriphClk + (BaudRate/2U))/BaudRate);
-}
+ static UART_Error_e UART_CheckErrors(UART_RegDef_t *pUARTx)
+ {
+    uint32_t sr = pUARTx->SR;
+    UART_Error_e error = UART_OK;
 
-void UART_PeriClockControl(UART_RegDef_t *pUARTx, uint8_t EnorDi)
-{
-    if(EnorDi == ENABLE)
+    if((sr & (1U << UART_SR_ORE)) != 0U)
     {
-        if(pUARTx == UART1)
-        {
-            UART1_PCLK_EN();
-        }else if(pUARTx == UART2)
-        {
-            UART2_PCLK_EN();
-        }else if(pUARTx == UART6)
-        {
-            UART6_PCLK_EN();
-        }
-    }else
+        error = UART_ERROR_ORE;
+        (void)pUARTx->DR;
+    }else if((sr & (1U << UART_SR_FE)) != 0U)
     {
-        if(pUARTx == UART1)
-        {
-            UART1_PCLK_DI();
-        }else if(pUARTx == UART2)
-        {
-            UART2_PCLK_DI();
-        }else if(pUARTx == UART6)
-        {
-            UART6_PCLK_DI();
-        }
+        error = UART_ERROR_FE;
+        (void)pUARTx->DR;
     }
-}
-
-void UART_Init(UART_Config_t *pUARTConfig)
-{
-    // temporary variable
-	uint32_t tempreg = 0;
-
-	// *** Configuration of CR1 **************************************
-	UART_PeriClockControl(pUARTConfig->pUARTx, ENABLE);
-
-	if(pUARTConfig->UART_Mode == UART_MODE_ONLY_RX)
-	{
-		tempreg |= (1 << UART_CR1_RE);
-	}else if(pUARTConfig->UART_Mode == UART_MODE_ONLY_TX)
-	{
-		tempreg |= (1 << UART_CR1_TE);
-	}else if(pUARTConfig->UART_Mode == UART_MODE_TXRX)
-	{
-		tempreg |= ((1 << UART_CR1_RE) | (1 << UART_CR1_TE));
-	}
-
-	tempreg |= (pUARTConfig->UART_WordLength << UART_CR1_M);
-
-	if(pUARTConfig->UART_ParityControl == UART_PARITY_EN_EVEN)
-	{
-		tempreg |= (1 << UART_CR1_PCE);
-	}else if(pUARTConfig->UART_ParityControl == UART_PARITY_EN_ODD)
-	{
-		tempreg |= (1 << UART_CR1_PCE);
-		tempreg |= (1 << UART_CR1_PS);
-	}
-	pUARTConfig->pUARTx->CR1 = tempreg;
-
-	// *** Configuration of CR2 **************************************
-	tempreg = 0;
-	tempreg |= (pUARTConfig->UART_NoOfStopBits << UART_CR2_STOP);
-	pUARTConfig->pUARTx->CR2 = tempreg;
-
-	// *** Configuration of CR3 **************************************
-	tempreg=0;
-
-    if ( pUARTConfig->UART_HWFlowControl == UART_HW_FLOW_CTRL_CTS)
+    else if ((sr & (1U << UART_SR_NE)) != 0U)
     {
-        tempreg |= ( 1 << UART_CR3_CTSE);
-    }else if (pUARTConfig->UART_HWFlowControl == UART_HW_FLOW_CTRL_RTS)
-    {
-        tempreg |= ( 1 << UART_CR3_RTSE);
-    }else if (pUARTConfig->UART_HWFlowControl == UART_HW_FLOW_CTRL_CTS_RTS)
-    {
-        tempreg |= ((1 << UART_CR3_CTSE) | ( 1 << UART_CR3_RTSE));
+        error = UART_ERROR_NE;
+        (void)pUARTx->DR;
     }
-    pUARTConfig->pUARTx->CR3 = tempreg;
-
-    // *** Configuration of BRR (Baudrate) ***********************
-    pUARTConfig->pUARTx->BRR = compute_uart_div(clock_get(), pUARTConfig->UART_Baud);
-}
-
-void UART_Init_table(const UART_Config_t *pUARTConfig, uint32_t Len)
-{
-    for(uint32_t i = 0; i < Len; i++)
+    else if ((sr & (1U << UART_SR_PE)) != 0U)
     {
-        UART_Init((UART_Config_t*)&pUARTConfig[i]);
+        error = UART_ERROR_PE;
+        (void)pUARTx->DR;
     }
-}
 
-void UART_DeInit(UART_RegDef_t *pUARTx)
-{
-    if      (pUARTx == UART1) UART1_REG_RESET();
-    else if	(pUARTx == UART2) UART2_REG_RESET();
-    else if	(pUARTx == UART6) UART6_REG_RESET();
-}
+    return error;
+ }
 
-void UART_write_byte(UART_RegDef_t *pUARTx, uint8_t data)
+static void configure_cr1(UART_RegDef_t *pUARTx, const UART_Config_t *pUARTConfig)
 {
-    pUARTx->DR = data;
-}
+    uint32_t cr1 = 0U;
 
-void UART_write(UART_RegDef_t *pUARTx, uint8_t *pTxBuffer, uint32_t Len)
-{
-    for(uint32_t i = 0; i < Len; i++)
+    switch(pUARTConfig->UART_Mode)
     {
-        while(!UART_GetFlagStatus(pUARTx, UART_FLAG_TXE));
-        UART_write_byte(pUARTx, pTxBuffer[i]);
+        case UART_MODE_ONLY_RX: cr1 |= (1U << UART_CR1_RE);                         break;
+        case UART_MODE_ONLY_TX: cr1 |= (1U << UART_CR1_TE);                         break;
+        case UART_MODE_TXRX:    cr1 |= (1U << UART_CR1_RE) | (1U << UART_CR1_TE);   break;
+        default:                                                                    break;
     }
-    while(!UART_GetFlagStatus(pUARTx, UART_FLAG_TC));
-}
 
-uint8_t UART_read_byte(UART_RegDef_t *pUARTx)
-{
-    uint8_t data = pUARTx->DR;
-    return data;
-}
+    cr1 |= (pUARTConfig->UART_WordLength << UART_CR1_M);
 
-uint8_t UART_GetFlagStatus(UART_RegDef_t *pUARTx , uint8_t FlagName)
-{
-	if(pUARTx->SR & FlagName)
-	{
-		return FLAG_SET;
-	}
-	return FLAG_RESET;
-}
-
-void UART_PeripheralControl(UART_RegDef_t *pUARTx, uint8_t EnorDi)
-{
-    if(EnorDi == ENABLE)
+    if(pUARTConfig->UART_ParityControl == UART_PARITY_EN_EVEN)
     {
-        pUARTx->CR1 |= (1 << UART_CR1_UE);
-    }else
-    {
-        pUARTx->CR1 &= ~(1 << UART_CR1_UE);
+        cr1 |= (1U << UART_CR1_PCE);
     }
+    else if (pUARTConfig->UART_ParityControl == UART_PARITY_EN_ODD)
+    {
+        cr1 |= (1U << UART_CR1_PCE) | (1U << UART_CR1_PS);
+    }
+
+    pUARTx->CR1 = cr1;
 }
 
-void UART_InterruptConfig(UART_RegDef_t *pUARTx, uint32_t registers, uint8_t EnorDi)
+static void configure_cr3(UART_RegDef_t *pUARTx, const UART_Config_t *pUARTConfig)
 {
-    pUARTx->CR1 &= ~(1 << 0);
-	if(EnorDi == ENABLE)
-	{
-		pUARTx->CR1 |= (registers);
-	}else
-	{
-		pUARTx->CR1 &= ~(registers);
-	}
+    uint32_t cr3 = 0U;
+
+    switch (pUARTConfig->UART_HWFlowControl)
+    {
+        case UART_HW_FLOW_CTRL_CTS:     cr3 |= (1U << UART_CR3_CTSE);                           break;
+        case UART_HW_FLOW_CTRL_RTS:     cr3 |= (1U << UART_CR3_RTSE);                           break;
+        case UART_HW_FLOW_CTRL_CTS_RTS: cr3 |= (1U << UART_CR3_CTSE) | (1U << UART_CR3_RTSE);   break;
+        default:                                                                                break;
+    }
+
+    pUARTx->CR3 = cr3;
 }
+
+/*********************************************************************
+ * Public API Implementations
+ *********************************************************************/
