45ed08fb7f46ea06b45cbb36d0df6412035411a4
slemanz <william_sleman@hotmail.com>
2026-02-23 04:45:54 -0300

    common(feat): add pwm and adc interface

--- common/Inc/interface/interface.h
+++ common/Inc/interface/interface.h
@@ -40,12 +40,41 @@ typedef struct
     void    (*write)(uint8_t value);
     uint8_t (*read)(void);
     void    (*toggle)(void);
+    void    (*set_mode)(uint8_t mode);     /**< Switch between IO_MODE_OUTPUT / IO_MODE_INPUT at runtime */
     void    (*deinit)(void);
-}IO_Interface_t;  
+} IO_Interface_t;
 
 #define IO_SET              1
 #define IO_RESET            0
+#define IO_MODE_OUTPUT      1
+#define IO_MODE_INPUT       0
 
 IO_Interface_t *IO_Interface_get(uint8_t pin);
+
+/************************************************************
+*                       ADC                                 *
+*************************************************************/
+
+typedef struct
+{
+    void     (*init)(void);
+    uint16_t (*read)(void);         /**< Returns raw ADC value */
+    void     (*deinit)(void);
+} ADC_Interface_t;
+
+ADC_Interface_t *ADC_Interface_get(uint8_t channel);
+
+/************************************************************
+*                       PWM                                 *
+*************************************************************/
+
+typedef struct
+{
+    void (*init)(void);
+    void (*set_duty)(float dutyPercent);    /**< 0.0 – 100.0 */
+    void (*deinit)(void);
+} PWM_Interface_t;
+
+PWM_Interface_t *PWM_Interface_get(uint8_t instance);
 
 #endif /* INC_INTERFACE_H_ */
