1c24c39dd1ed5f53a31d602e464fe8bce40a3197
slemanz <william_sleman@hotmail.com>
2026-03-01 08:29:27 -0300

    interface(refact): change timebase to global funcs

--- interface/Src/interface_timebase.c
+++ interface/Src/interface_timebase.c
@@ -1,17 +1,25 @@
 #include "interface/interface.h"
 #include "driver_systick.h"
-#include "driver_clock.h"
 
-uint8_t timebase_is_init = 0;
+static uint8_t s_is_init = 0u;
 
-const static timebase_interface_t timer_systick = {
-    .get = ticks_get,
-    .deinit = NULL
-};
-
-timebase_interface_t *timebase_get(void)
+static void timebase_ensure_init(void)
 {
-    if(timebase_is_init == 0) systick_init(1000);
+    if (!s_is_init)
+    {
+        systick_init(1000u);
+        s_is_init = 1u;
+    }
+}
 
-    return (timebase_interface_t*)&timer_systick;
+uint64_t timebase_get(void)
+{
+    timebase_ensure_init();
+    return ticks_get();
+}
+
+void timebase_deinit(void)
+{
+    // todo
+    s_is_init = 0u;
 }
