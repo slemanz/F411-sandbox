4a54c7f0d5f344fe8451f31170e4704aa0fa2ec4
slemanz <william_sleman@hotmail.com>
2026-03-07 13:55:15 -0300

    external(create)

--- tests/mocks/mock_interface_io.h
+++
@@ -1,9 +0,0 @@
-#ifndef MOCK_INTERFACE_IO_H_
-#define MOCK_INTERFACE_IO_H_
-
-#include <stdint.h>
-
-void mock_io_set_pin_level  (uint8_t pin_id, uint8_t level);
-void mock_io_reset_levels   (void);
-
-#endif /* MOCK_INTERFACE_IO_H_ */
